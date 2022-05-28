e46f1c52c34ef0f75e68127460d0af64e20edda7
Paolo589 <kappa589@hotmail.it>
2022-05-28 12:25:09 +0200

    header a scomparsa

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,43 @@ import Link from "next/link"
 import SwipableMenu from "./menu/SwipableMenu"
 
 import Logo from "./menu/Logo"
+import { useEffect, useState } from "react";
 
 
-const Header = (): JSX.Element => (
-  <header className="header">
+const Header = () => {
+
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') { 
+      if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
+        setShow(false); 
+      } else { // if scroll up show the navbar
+        setShow(true);  
+      }
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY); 
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
+
+ return( <header className={`header ${!show && 'hidden'}`}>
     <Logo></Logo>
     <SwipableMenu />
-  </header>
-)
+  </header>);
+};
 
 export default Header
