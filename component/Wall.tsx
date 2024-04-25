09eba25dffc3207ddda8ad1b88fc5a21e6ced999
dariovettura <d.vettura.wd@gmail.com>
2024-04-25 11:41:14 +0200

    okoko

--- component/Wall.tsx
+++ component/Wall.tsx
@@ -133,7 +133,7 @@ function Wall(props: any) {
 
 
     (function () {
-   
+
       const elmHamburger: any = document.querySelector(".hamburger");
       const menulink1: any = document.querySelector(".menulink1");
       const menulink2: any = document.querySelector(".menulink2");
@@ -163,11 +163,13 @@ function Wall(props: any) {
       const elmOverlay: any = document.querySelector(".shape-overlays");
       const overlay: any = new ShapeOverlays(elmOverlay);
 
+    
+
       // elmHamburger?.addEventListener("click", () => {
       //   if (overlay.isAnimating) {
       //     return false;
       //   }
-      //   overlay.toggle();
+      // //   overlay.toggle();
       //   if (overlay.isOpened === true) {
       //     elmHamburger?.classList.add("is-opened-navi");
       //     bodys.classList.add("overflows");
@@ -180,7 +182,7 @@ function Wall(props: any) {
       //       gNavItems[i].classList.add("is-opened");
       //     }
       //   } else {
-      //     elmHamburger.classList.remove("is-opened-navi");
+      // //     elmHamburger.classList.remove("is-opened-navi");
       //     bodys.classList.remove("overflows");
       //     cubo.classList.remove("rotating");
       //     removeRooot();
@@ -194,16 +196,16 @@ function Wall(props: any) {
       //   if (overlay.isAnimating) {
       //     return false;
       //   }
-      //   overlay.toggle();
+      // //   overlay.toggle();
       //   if (overlay.isOpened === true) {
-      //     elmHamburger.classList.add("is-opened-navi");
+      // //     elmHamburger.classList.add("is-opened-navi");
       //     bodys.classList.add("overflows");
       //     addRooot();
       //     for (var i = 0; i < gNavItems.length; i++) {
       //       gNavItems[i].classList.add("is-opened");
       //     }
       //   } else {
-      //     elmHamburger.classList.remove("is-opened-navi");
+      // //     elmHamburger.classList.remove("is-opened-navi");
       //     bodys.classList.remove("overflows");
       //     removeRooot();
       //     for (var i = 0; i < gNavItems.length; i++) {
@@ -215,16 +217,16 @@ function Wall(props: any) {
       //   if (overlay.isAnimating) {
       //     return false;
       //   }
-      //   overlay.toggle();
+      // //   overlay.toggle();
       //   if (overlay.isOpened === true) {
-      //     elmHamburger.classList.add("is-opened-navi");
+      // //     elmHamburger.classList.add("is-opened-navi");
       //     bodys.classList.add("overflows");
       //     addRooot();
       //     for (var i = 0; i < gNavItems.length; i++) {
       //       gNavItems[i].classList.add("is-opened");
       //     }
       //   } else {
-      //     elmHamburger.classList.remove("is-opened-navi");
+      // //     elmHamburger.classList.remove("is-opened-navi");
       //     bodys.classList.remove("overflows");
       //     removeRooot();
       //     for (var i = 0; i < gNavItems.length; i++) {
@@ -236,16 +238,16 @@ function Wall(props: any) {
       //   if (overlay.isAnimating) {
       //     return false;
       //   }
-      //   overlay.toggle();
+      // //   overlay.toggle();
       //   if (overlay.isOpened === true) {
-      //     elmHamburger.classList.add("is-opened-navi");
+      // //     elmHamburger.classList.add("is-opened-navi");
       //     bodys.classList.add("overflows");
       //     addRooot();
       //     for (var i = 0; i < gNavItems.length; i++) {
       //       gNavItems[i].classList.add("is-opened");
       //     }
       //   } else {
-      //     elmHamburger.classList.remove("is-opened-navi");
+      // //     elmHamburger.classList.remove("is-opened-navi");
       //     bodys.classList.remove("overflows");
       //     removeRooot();
       //     for (var i = 0; i < gNavItems.length; i++) {
@@ -303,7 +305,9 @@ function Wall(props: any) {
       },
     };
 
-   (function () {
+
+
+    (function () {
       var el = document.createElement("div"),
         transformProps = "transform WebkitTransform MozTransform OTransform msTransform".split(" "),
         transformProp = support(transformProps),
@@ -411,14 +415,14 @@ function Wall(props: any) {
       var scrollHandler = function (evt: any) {
         var delta = Math.max(-1, Math.min(1, (evt.wheelDelta || -evt.detail))); // Get delta for scrolling
         var scrollFactor = 50; // Adjust this value to control scroll speed
-    
+
         // Update viewport position based on scroll direction
         viewport.move({
-            y: viewport.y + delta * scrollFactor
+          y: viewport.y + delta * scrollFactor
         });
-    
+
         evt.preventDefault(); // Prevent default scroll behavior
-    };
+      };
 
       var endHandler = function () {
         document.removeEventListener("mousemove", moveHandler);
@@ -440,13 +444,13 @@ function Wall(props: any) {
         document.addEventListener("mouseup", endHandler);
         document.addEventListener("touchend", endHandler);
 
-       
+
       };
 
       document.addEventListener("mousedown", startHandler);
       document.addEventListener("touchstart", startHandler);
       document.addEventListener("mousewheel", scrollHandler);
-      document.addEventListener("DOMMouseScroll", scrollHandler); 
+      document.addEventListener("DOMMouseScroll", scrollHandler);
     }
     )();
 
@@ -459,141 +463,166 @@ function Wall(props: any) {
     // })
   };
 
+  const removeIntro = () => {
+    const bodys: any = document.querySelector("body");
+    const gNavItems: any = document.querySelectorAll(".global-menu__item");
+    const rooot: any = document.querySelectorAll(".glass");
+    const rooot2: any = document.querySelectorAll(".sub-glass");
+    const removeRooot = () => {
+      for (var i = 0; i < rooot.length; i++) {
+        rooot[i].classList.remove("glassVisible");
+      }
+      for (var i = 0; i < rooot2.length; i++) {
+        rooot2[i].classList.remove("sub-glass");
+      }
+    };
+    removeRooot();
+    bodys.classList.remove("overflows");
+    for (var i = 0; i < gNavItems.length; i++) {
+      gNavItems[i].classList.remove("is-opened");
+    }
+  }
 
-  React.useEffect(() => {
-    menu(), []
-  })
 
+
+
+
+  const init =
+  React.useCallback(async (node: any) => {
+    if (node) {
+      menu()
+    }
+  }, []);
 
   return (
-    <div className="demo-3">
+    <div ref={init} className="demo-3">
       <div className="main main--demo-3">
         <div className="content content--demo-3">
           <div className="global-menu">
             <div className="global-menu__wrap">
               <div className="global-menu__item global-menu__item--demo-3 is-opened">
                 <div >
-                  <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ velocity: 3 ,delay: 2,}}>
-                  <article className="viewport nascostos">
-                   
-                    <section className="cube rotating">
-                      <div>
-                        <div className="container-fluid">
-                          <div className="row text-center">
-                            <div className="col-12">
-                              <h2><span></span></h2>
+                  <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ velocity: 3, delay: 2, }}>
+                    <article className="viewport nascostos">
+
+                      <section className="cube rotating">
+                        <div>
+                          <div className="container-fluid">
+                            <div className="row text-center">
+                              <div className="col-12">
+                                <h2><span></span></h2>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                      <div>
-                      <div className="container-fluid">
-                          <div className="row text-center">
-                            <div className="col-12">
-                              <Link href="#about"
-                                passHref={true}>
-                                <h2 className="menulink2">ABOUT<span>.</span></h2>
-                              </Link>
+                        <div>
+                          <div className="container-fluid">
+                            <div className="row text-center">
+                              <div className="col-12">
+                                <Link  href="#about"
+                                  passHref={true}>
+                                  <a onClick={removeIntro} className="menulink2">ABOUT<span>.</span></a>
+                                </Link>
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div>
+                          <div className="container-fluid">
+                            <div className="row text-center">
+                              <div className="col-12">
+                                <Link href="#works"
+                                  passHref={true}>
+                                  <a onClick={removeIntro} className="menulink1">WORKS<span>.</span></a>
+                                </Link>
+                              </div>
                             </div>
                           </div>
                         </div>
-                       
-                      </div>
-                      <div>
-                        <div className="container-fluid">
-                          <div className="row text-center">
-                            <div className="col-12">
-                              <Link href="#works"
-                                passHref={true}>
-                                <h2 className="menulink1">WORKS<span>.</span></h2>
-                              </Link>
+                        <div>
+                          <div className="container-fluid">
+                            <div className="row text-center">
+                              <div className="col-12 log">
+                                <Link href="#"
+                                  passHref={true}>
+                                  <a>D<span>.</span></a>
+                                </Link>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                      <div>
-                      <div className="container-fluid">
-                          <div className="row text-center">
-                            <div className="col-12 log">
-                              <Link href="#"
-                                passHref={true}>
-                                <h2>D<span>.</span></h2>
-                              </Link>
+                        <div>
+                          <div className="container-fluid">
+                            <div className="row text-center">
+                              <div className="col-12">
+                                <Link href="#contacts"
+                                  passHref={true}>
+                                  <a onClick={removeIntro} className="menulink3">CONTACTS<span>.</span></a>
+                                </Link>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                      <div>
-                        <div className="container-fluid">
-                          <div className="row text-center">
-                            <div className="col-12">
-                              <Link href="#contacts"
-                                passHref={true}>
-                                <h2 className="menulink3">CONTACTS<span>.</span></h2>
-                              </Link>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
 
 
-                      <div>
-                        <div className="container-fluid">
-                          <div className="row text-center">
-                            <div className="col-12">
-                              <h2><span></span></h2>
+                        <div>
+                          <div className="container-fluid">
+                            <div className="row text-center">
+                              <div className="col-12">
+                                <h2><span></span></h2>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                    </section>
-                 
-                  
-                  </article>
+                      </section>
+
+
+                    </article>
                   </motion.div>
                   <section className='hero-text'>
-                      <div className="container-fluid menutext hero2 nascostos">
-                        <div className="row rigahero text-center">
-                          <div className="col-12 colhero">
-                            <h2>WEB DEV PORTFOLIO</h2>
-                            <h3>Dario Vettura</h3>
-                          </div>
-                        </div>
-                       
-                        <div
-                          className="row icone text-center justify-content-center"
-                        >
-                          <div className="col-3 ">
-                            <Link
-                              passHref={true}
-                              href="https://mail.google.com/mail/?view=cm&source=mailto&to=d.vettura.wd@gmail.com"
-                            ><div className="icona2 face"></div></Link>
-                          </div>
-                          <div className="col-3 ">
-                            <Link
-                              // target="_blank"
-                              href="http://m.me/dario.vettura"
-                              passHref={true}
-                            >
-                              <div className="icona2 insta">
-                              </div >
-                            </Link>
-                          </div>
-                          <div className="col-3 ">
-                            <Link
-                              href="http://m.me/dario.vettura"
-                              passHref={true}
-                            >
-                              <div className="icona2 linkedin"></div></Link>
-                          </div>
-                          <div className="col-12 sottitolo2">
-                            <p>
-                              © Dario Vettura 2020<span>.</span>
-                            </p>
-                          </div>
+                    <div className="container-fluid menutext hero2 nascostos">
+                      <div className="row rigahero text-center">
+                        <div className="col-12 colhero">
+                          <h2>WEB DEV PORTFOLIO</h2>
+                          <h3>Dario Vettura</h3>
                         </div>
                       </div>
-                    </section>
+
+                      <div
+                        className="row icone text-center justify-content-center"
+                      >
+                        <div className="col-3 ">
+                          <Link
+                            passHref={true}
+                            href="https://mail.google.com/mail/?view=cm&source=mailto&to=d.vettura.wd@gmail.com"
+                          ><div className="icona2 face"></div></Link>
+                        </div>
+                        <div className="col-3 ">
+                          <Link
+                            // target="_blank"
+                            href="http://m.me/dario.vettura"
+                            passHref={true}
+                          >
+                            <div className="icona2 insta">
+                            </div >
+                          </Link>
+                        </div>
+                        <div className="col-3 ">
+                          <Link
+                            href="http://m.me/dario.vettura"
+                            passHref={true}
+                          >
+                            <div className="icona2 linkedin"></div></Link>
+                        </div>
+                        <div className="col-12 sottitolo2">
+                          <p>
+                            © Dario Vettura 2020<span>.</span>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
                 </div>
               </div>
             </div>
