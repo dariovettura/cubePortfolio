2259edfb1ce678d9801692f9302f93fba0ea1b03
Dario Vettura <73988414+dariovettura@users.noreply.github.com>
2025-06-20 20:07:46 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { motion,} from "framer-motion"
 import About from '../component/About';
 import Contacts from '../component/Contacts';
 import Home from '../component/Home';
-
+import Axios from 'axios';
 
 
 
@@ -14,6 +14,16 @@ interface Props {
 }
 
 const Casa: NextPage<Props> = ({ posts }) => {
+
+  useEffect(() => {
+    Axios.get('https://beergarden.free.nf/api/categories')
+      .then((response) => {
+        console.log('Risposta API categorie:', response.data);
+      })
+      .catch((error) => {
+        console.error('Errore nella chiamata API categorie:', error);
+      });
+  }, []);
 
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ velocity: 50 }} id="card-list-container" >
